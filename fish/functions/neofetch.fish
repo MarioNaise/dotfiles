9b38388f6021523b2475042deba3a71166009bea
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2024-07-06 14:06:33 +0200

    Remove neofetch alias

--- fish/functions/neofetch.fish
+++
@@ -1,4 +0,0 @@
-function neofetch --description 'alias neofetch neofetch --colors 6 6 6 6 --ascii_colors 6 6 6 6 6 6'
- command neofetch --colors 6 6 6 6 --ascii_colors 6 6 6 6 6 6 $argv
-        
-end
