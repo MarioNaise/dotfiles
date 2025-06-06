14147674a17fba910db9456549ee80ddc501d98b
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2025-06-06 11:23:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo Enter directory name
-read DIR
-
 DIR=${DIR/#\~/$HOME}
 DIR=${DIR/#\$HOME/$HOME}
 
 for i in nvim tmux fish bat; do
-  ln -s $PWD/$i $DIR/$i
+  ln -s $PWD/$i $HOME/.config/$i
 done
 ln -s $PWD/.gitconfig ~/.gitconfig
+
+bat cache --build
