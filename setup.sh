47c9017555dacc6f451fd0239fda6eea3bdd5ff8
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2025-06-18 16:27:32 +0200

    ncspot config

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ options=(
   kitty
   waybar
   wofi
+  ncspot
   xsettingsd
   gtk-3.0
   gtk-4.0
@@ -21,7 +22,7 @@ options=(
 select opt in "${options[@]}"; do
   if [[ -z $opt ]]; then
     break
-  elif [[ $opt == ".gitconfig" || $opt == ".gtkrc-2.0" || $opt == ".themes" ]]; then
+  elif [[ $opt == ".gitconfig" || $opt == ".gtkrc-2.0" ]]; then
     ln -s $PWD/$opt $HOME/$opt
   elif [[ $opt == "bat" ]]; then
     ln -s $PWD/$opt $HOME/.config/$opt
