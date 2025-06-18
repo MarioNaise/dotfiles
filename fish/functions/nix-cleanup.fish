30120203edc3e5172a9953c31627e4f5f1161b63
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2025-06-18 20:40:21 +0200

    NixOS adjustments

--- fish/functions/nix-cleanup.fish
+++ fish/functions/nix-cleanup.fish
@@ -1,5 +1,4 @@
 function nix-cleanup
-    sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system
     nix-collect-garbage -d # nix store gc
-    nix store optimise --extra-experimental-features nix-command
+    nix store optimise
 end
