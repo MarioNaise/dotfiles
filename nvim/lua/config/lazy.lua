4e68c84b1dca067182319ce1c13addca29a1e859
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2025-09-24 12:07:48 +0200

    Add "sh" filetype to all .env.* files

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -92,3 +92,5 @@ vim.api.nvim_set_hl(0, "@lsp.typemod.method.defaultLibrary.javascriptreact", {})
 -- for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
 --   vim.api.nvim_set_hl(0, group, {})
 -- end
+
+vim.filetype.add({ pattern = { [".env.*"] = "sh" }})
