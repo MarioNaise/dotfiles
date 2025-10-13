e31c808f7a22179780993fea90bd5380e2c4e086
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2025-10-13 19:42:29 +0200

    Update config.fish and variables

--- fish/config.fish
+++ fish/config.fish
@@ -12,15 +12,28 @@ if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
     . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
 end
 
+# go
+if test -d ~/dev/go
+    set -xg GOPATH ~/dev/go
+    set -xg GOBIN $GOPATH/bin
+    fish_add_path --path $GOBIN
+end
+
 # bun
-set -xg BUN_INSTALL "$HOME/.bun"
-set -xg PATH $BUN_INSTALL/bin $PATH
+if test -d ~/.bun
+    set -xg BUN_INSTALL ~/.bun
+    fish_add_path --path $BUN_INSTALL/bin
+end
+
+# yarn
+if test -d ~/.yarn
+    fish_add_path --path ~/.yarn
+end
 
 # jetbrains
-set -xg PATH ~/.jetbrains $PATH
-
-# apache maven
-set -xg PATH ~/dev/apache-maven-3.9.0/bin $PATH
+if test -d ~/.jetbrains
+    fish_add_path --path ~/.jetbrains
+end
 
 # gcloud
 if test -f ~/dev/google-cloud-sdk/path.fish.inc
@@ -32,11 +45,15 @@ if test -f ~/dev/google-cloud-sdk/completion.fish.inc
 end
 
 # zoxide
-zoxide init fish | source
+if command -q zoxide
+    zoxide init fish | source
+end
 
 # fzf
-fzf --fish | source
-source ~/.config/fish/functions/fzf-file-widget.fish
-source ~/.config/fish/functions/fzf-cd-widget.fish
-bind \cgh fzf-git-log-widget
-bind \cgg fzf-grep-widget
+if command -q fzf
+    fzf --fish | source
+    source ~/.config/fish/functions/fzf-file-widget.fish
+    source ~/.config/fish/functions/fzf-cd-widget.fish
+    bind \cgh fzf-git-log-widget
+    bind \cgg fzf-grep-widget
+end
