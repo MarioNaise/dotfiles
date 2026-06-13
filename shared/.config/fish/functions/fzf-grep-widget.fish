359b239a74c02099bc2362174398d032c40d91cf
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2026-06-13 10:42:44 +0200

    Fix grep widget

--- shared/.config/fish/functions/fzf-grep-widget.fish
+++ shared/.config/fish/functions/fzf-grep-widget.fish
@@ -1,31 +1,54 @@
 function fzf-grep-widget
-    set -l commandline (__fzf_parse_commandline)
+    set -l result (
+        fzf -m --disabled \
+            --bind 'start:reload(fd -t f -H -E .git)' \
+            --bind 'change:reload(
+                if test -z {q}
+                    fd -t f -H -E .git
+                else
+                    rg -.S --line-number --color=always -g "!.git/**" -- {q} .
+                end
+            )' \
+            --preview='
+                if test -z {q}
+                    bat {} --color=always
+                else
+                    set -l file (string split ":" -- {})[1]
+                    set -l line (string split ":" -- {})[2]
 
-    begin
-        set result (fzf -m --disabled --bind 'change:reload(if test -z {q};fd -t f -H -E ".git";
-          else; rg -S --line-number --color=always {q};end)' \
-          --preview='if test -z {q};bat {} --color=always; else; set -l line (string split ":" {})[2];
-          bat (string split ":" {})[1] \
-            -r (if test -z $line; set line 0; end; if test $line -lt 20; echo 0; else; echo (math $line-20); end): \
-            -H $line --color=always;end' \
-        --tmux 80% --ansi)
-    end
+                    if test -z "$line"
+                        set line 0
+                    end
 
-    if [ -z "$result" ]
+                    set -l start 0
+                    if test "$line" -ge 20
+                        set start (math $line - 20)
+                    end
+
+                    bat "$file" -r "$start": -H "$line" --color=always
+                end
+            ' \
+            --tmux 80% --ansi
+    )
+
+    if test -z "$result"
         commandline -f repaint
         return
-    else
-        commandline -t ""
     end
-    commandline -it -- 'vim '
+
+    commandline -t ""
+    commandline -it -- "vim "
+
     for i in $result
-        set -l file (string split ":" $i[1])[1]
-        set -l line (string split ":" $i[1])[2]
-        commandline -it -- $file
-        commandline -it -- ' '
-        if test -n $line
-            commandline -it -- +$line' '
+        set -l file (string split ":" -- $i)[1]
+        set -l line (string split ":" -- $i)[2]
+
+        commandline -it -- "$file "
+
+        if test -n "$line"
+            commandline -it -- "+$line "
         end
     end
+
     commandline -f repaint
 end
