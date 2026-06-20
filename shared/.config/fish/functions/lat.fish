01c66548ad774de5c72d086549c21019bf07e3c5
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2026-06-20 16:45:58 +0200

    Add lt/lat alias

---
+++ shared/.config/fish/functions/lat.fish
@@ -0,0 +1,3 @@
+function lat --wraps='eza -la --total-size' --description 'alias lat eza -la --total-size'
+    eza -la --total-size $argv
+end
---
+++ shared/.config/fish/functions/lt.fish
@@ -0,0 +1,3 @@
+function lt --wraps='eza -l --total-size' --description 'alias lt eza -l --total-size'
+    eza -l --total-size $argv
+end
