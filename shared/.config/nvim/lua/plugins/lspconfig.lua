3f595ab8fcab587e29967049a2e9c3b220b49b09
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2026-06-28 22:09:56 +0200

    zls build on save

--- shared/.config/nvim/lua/plugins/lspconfig.lua
+++ shared/.config/nvim/lua/plugins/lspconfig.lua
@@ -2,5 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      zls = {
+        settings = {
+          enable_build_on_save = true,
+        },
+      },
+    },
   },
 }
